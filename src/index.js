001e03041f0cd060382854cb0cb756e32f3e666f
Roman Rädle <contact@romanraedle.com>
2017-11-28 18:25:43 +0100

    Configured webpack to generate transformer.js and transformer.min.js Added four examples to showcase transformer.js Documentation is missing

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import Point from './utils/point';
+import Matrix from './utils/matrix';
+import TranslateTransform from './transforms/translate-transform';
+import RotateTransform from './transforms/rotate-transform';
+import ScaleTransform from './transforms/scale-transform';
+import TransformGroup from './transforms/transform-group';
+import Transformer from './transformer';
+
+import { bind } from './extensions/bind';
+import { hammerize } from './extensions/hammerize';
+
+// Object.assign(Transformer, {
+
+//   // utils
+//   Point,
+//   Matrix,
+
+//   // transforms
+//   TranslateTransform,
+//   RotateTransform,
+//   ScaleTransform,
+//   TransformGroup,
+
+//   // extensions
+//   hammerize
+// });
+
+export {
+    // utils
+    Point,
+    Matrix,
+
+    // transforms
+    TranslateTransform,
+    RotateTransform,
+    ScaleTransform,
+    TransformGroup,
+
+    // extensions
+    bind,
+    hammerize
+}
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+
+// package configuration
+const pkg = require('./package.json');
+
+// webpack
+const webpack = require('webpack');
+
+// webpack plugins
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// const banner = {
+//     banner: "some banner\n\nlinebreaks\nonemore", // the banner as string, it will be wrapped in a comment
+//     // raw: boolean, // if true, banner will not be wrapped in a comment
+//     // entryOnly: boolean, // if true, the banner will only be added to the entry chunks
+//     // test: string | RegExp | Array,
+//     // include: string | RegExp | Array,
+//     // exclude: string | RegExp | Array,
+// };
+
+module.exports = {
+    entry: {
+        "transformer": './src/index.js',
+        "transformer.min": './src/index.js'
+    },
+    // devtool: 'source-map',
+    // devServer: {
+    //     contentBase: './dist',
+    //     hot: true
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist']),
+        //     new webpack.BannerPlugin(fs.readFileSync('./_LICENSE', 'utf-8')),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true,
+            sourceMap: true
+        }),
+        //     new HtmlWebpackPlugin({
+        //         title: `${pkg.name} v${pkg.version}`,
+        //     }),
+        //     new webpack.NamedModulesPlugin(),
+        //     new webpack.HotModuleReplacementPlugin()
+    ],
+    externals: ['hammerjs'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: 'Transformer',
+        libraryTarget: 'window'
+    }
+};
